f18b2fb28f23171af414ffb93f1b729057c2e548
Spencer Marshall <spencerthomasmarshall@gmail.com>
2026-07-03 19:44:18 -0400

    improvements to `gd`

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,5 +1,21 @@
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
 
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(ev)
+    local opts = { buffer = ev.buf }
+    vim.keymap.set("n", "gd", function()
+      vim.cmd("vsplit")
+      vim.lsp.buf.definition()
+    end, opts)
+    vim.keymap.set("n", "K",          vim.lsp.buf.hover,       opts)
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename,      opts)
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "gr",         vim.lsp.buf.references,  opts)
+    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+  end,
+})
+
 require("mason").setup()
 require("mason-lspconfig").setup({
 	automatic_installation = true,
