6f23ccdac8d384638bf8b675a1f5bd41f3c3f84b
Spencer Marshall <spencerthomasmarshall@gmail.com>
2026-07-05 17:34:02 -0400

    switch to nvim-treesitter main branch

--- lua/plugins/configs.lua
+++ lua/plugins/configs.lua
@@ -39,10 +39,23 @@ return {
    },
    {
       "nvim-treesitter/nvim-treesitter",
+      branch = "main",
       lazy = false,
-      config = function()
-         require("nvim-treesitter").setup({
-            ensure_installed = { "lua", "rust", "toml" },
+      build = ":TSUpdate",
+      init = function()
+         local ensure_installed = { "lua", "rust", "toml" }
+         local installed = require("nvim-treesitter.config").get_installed()
+         local to_install = vim.iter(ensure_installed)
+            :filter(function(p) return not vim.tbl_contains(installed, p) end)
+            :totable()
+         if #to_install > 0 then
+            require("nvim-treesitter").install(to_install)
+         end
+
+         vim.api.nvim_create_autocmd("FileType", {
+            callback = function()
+               pcall(vim.treesitter.start)
+            end,
          })
       end,
    },
