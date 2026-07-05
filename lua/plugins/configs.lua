613fbfd07583e57105475eb6e67bf2d240149628
Spencer Marshall <spencerthomasmarshall@gmail.com>
2026-07-05 16:32:15 -0400

    autopairing

--- lua/plugins/configs.lua
+++ lua/plugins/configs.lua
@@ -46,6 +46,15 @@ return {
          })
       end,
    },
+   {
+      "windwp/nvim-autopairs",
+      event = "InsertEnter",
+      config = function()
+         require("nvim-autopairs").setup()
+         local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+         require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+      end,
+   },
    { "williamboman/mason.nvim" },
    {
       "williamboman/mason-lspconfig.nvim",
